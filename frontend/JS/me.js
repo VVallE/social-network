8606239a33d21dd5b70759ba528061af497abefe
Moldy <Moldy@mx>
2025-06-26 17:31:24 +0300

    More updates

--- frontend/JS/me.js
+++ frontend/JS/me.js
@@ -1,15 +1,19 @@
-function getInfoAboutSelf() {
+async function getInfoAboutSelf() {
     let jwt_token = localStorage.getItem("jwt_key");
-    fetch('http://localhost:5000/api/me', {
+    let response = await fetch('http://localhost:5000/api/me', {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${jwt_token}`
         }
     })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+    let data = await response.json();
+    return data;
 }
-
-getInfoAboutSelf()
+document.addEventListener("DOMContentLoaded", async function () {
+let me = await getInfoAboutSelf();
+    document.getElementById("about-me").innerHTML = `
+        <h4 class="about-username" id="about-me-username">${me['username']}</h4>
+        <h5 id="about-me-email"></h5>
+        `
+});
--- frontend/JS/retreiveposts.js
+++ frontend/JS/retreiveposts.js
@@ -10,6 +10,24 @@ async function getAllPosts(mood) {
     console.log(data);
     return data;
 }
+
+async function likePost(postID) {
+    let jwt_key = localStorage.getItem("jwt_key")
+    let response = await fetch(`http://localhost:5000/api/posts/${postID}/like`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt_key}`
+        }, 
+        body: {
+            post_id: postID
+        }
+    });
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
+
 let postnum = 0;
 
 document.addEventListener("DOMContentLoaded", async function () {
@@ -20,15 +38,18 @@ document.addEventListener("DOMContentLoaded", async function () {
         const postElement = document.createElement("div");
         postElement.className = "post";
         postElement.id = postnum;
-        postElement.onclick = function () {
-            window.location.href = `http://127.0.0.1:5500/frontend/HTML/post.html?id=${postnum}`
-        }
+
         postElement.innerHTML = `
             <div class="post-author">Author: ${post.author_id}</div>
             <div class="post-content">${post.content}</div>
             <div class="post-date">Posted on: ${new Date(post.created_at).toLocaleString()}</div>
+            <div class="post-like" id="${postnum}-like"><button>Like!</button></div>
         `;
         feed.appendChild(postElement);
+
+        document.getElementById(`${postnum}-like`).addEventListener("click", async function () {
+            likePost(postnum);
+        });
         postnum += 1;
     });
 });
